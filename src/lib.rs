14355ccfc7da566cb4d442ab70bc1937d6b9d2d0
Jacob Froman <jacob.h.froman@gmail.com>
2017-02-20 21:08:14 -0600

    added group_max param

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 
-pub fn allocate_teams<'a>(names: &'a Vec<String>) -> Vec<Vec<Vec<String>>> {
-    let inner = Vec::new();
+pub fn allocate_teams<'a>(names: &'a Vec<&str>, group_max: usize) -> Vec<Vec<Vec<&'a str>>> {
+    let mut inner = Vec::new();
+    for name in names {
+        inner.push(*name);
+    }
     let middle = vec![inner];
     let outer = vec![middle];
     outer
@@ -11,8 +14,13 @@ mod test {
     use allocate_teams;
 
     #[test]
-    fn one_iteration_one_member() {
-        assert_eq!(vec![vec![vec!["Bob".to_string()]]],
-                   allocate_teams(&vec!["Bob".to_string()]));
+    fn one_iteration_max_one_one_member() {
+        assert_eq!(vec![vec![vec!["Alice"]]], allocate_teams(&vec!["Alice"], 1));
+    }
+
+    #[test]
+    fn one_iteration_max_one_two_members() {
+        assert_eq!(vec![vec![vec!["Alice"], vec!["Bob"]]],
+                   allocate_teams(&vec!["Alice", "Bob"], 1));
     }
 }
